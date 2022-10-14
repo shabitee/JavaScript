5bb3a98e281b0921a954b12892d56f361624cd34
shabitee <94993051+shabitee@users.noreply.github.com>
2022-10-14 21:58:29 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+//Exercise1
+let myAge=38;
+console.log(myAge);
+
+
+//exercise2
+let numberOfRabbits=2;
+answer=numberOfRabbits *3;
+console.log(answer);
+
+
+//Exercise3
+let bodmasExample=3
+answer= (bodmasExample 
++ 5) % 6;
+console.log(answer);
+
+//Exercise4
+randomNumber = Math.random();
+console.log(randomNumber);
+
+
+//Exercise5
+let myName ="Azeezah";
+console.log(myName);
+let myNewAge=37;
+console.log(myNewAge);
+let herName = prompt("What is your name");
+console.log(herName);
+
+
+//Exercise6
+
+const firstName ="Ubaydullah";
+const lastName= "Bidmos";
+
+let message ="Welcome";
+console.log(message+" "+firstName+" "+lastName);
+let message2 = prompt("How can I help you?");
+console.log(message2);
+
+
+
+
+
+
+
